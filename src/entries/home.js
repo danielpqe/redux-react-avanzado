28a8ba73106fae507dea43c3d3560275e1dd1702
danielpqe <danielpqe@gmail.com>
2018-12-03 12:42:56 -0500

    añadiendo multiples reducers

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
-import data from '../api.json';
+// import data from '../api.json';
+import data from '../schemas/index'
 // console.log('Hola mundo!' )
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from '../reducer/data'
 
+// console.log(normalizedData)
+// console.log(data)
+
 const initialState= {
     data: {
-        ...data
+        // ...data
+        entities: data.entities,
+        categories: data.result.categories
     },
     search: []
 
@@ -23,7 +29,7 @@ const store = createStore(
 
 );
 
-console.log(store.getState())
+// console.log(store.getState())
 const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
